1dc1b2296aac6088abaa5049b2bf61cfaa5b9dc9
Jim Shu <jim.shu@sifive.com>
2025-04-15 15:01:03 +0800

    target/riscv: Add hard-coded CPU states of RISCV Worlds extension
    Add hard-coded state of Worlds extensions:
    
    - 'pmwid' is the M-mode WID of CPU.
    - 'pmwidlist' is the list of allowed WID value of 'mwid' CSR.
    - 'pmlwidlist' is the list of allowed WID value of 'mlwid' CSR.
    
    These CPU states can be set by CPU option, or can be set by machine code
    via newly added APIs. If we want different Worlds configs of each CPU, we
    should set it by machine code.
    
    Signed-off-by: Jim Shu <jim.shu@sifive.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -649,6 +649,9 @@ void riscv_cpu_set_aia_ireg_rmw_fn(CPURISCVState *env, uint32_t priv,
                                    void *rmw_fn_arg);
 
 RISCVException smstateen_acc_ok(CPURISCVState *env, int index, uint64_t bit);
+void riscv_cpu_set_wg_pmwid(CPURISCVState *env, uint32_t pmwid);
+void riscv_cpu_set_wg_pmlwidlist(CPURISCVState *env, uint32_t pmlwidlist);
+void riscv_cpu_set_wg_pmwidlist(CPURISCVState *env, uint32_t pmwidlist);
 #endif /* !CONFIG_USER_ONLY */
 
 void riscv_cpu_set_mode(CPURISCVState *env, target_ulong newpriv, bool virt_en);
--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -131,6 +131,32 @@ bool riscv_env_smode_dbltrp_enabled(CPURISCVState *env, bool virt)
 #endif
 }
 
+#ifndef CONFIG_USER_ONLY
+void riscv_cpu_set_wg_pmwid(CPURISCVState *env, uint32_t pmwid)
+{
+    CPUState *cs = env_cpu(env);
+    RISCVCPU *cpu = RISCV_CPU(cs);
+
+    cpu->cfg.pmwid = pmwid;
+}
+
+void riscv_cpu_set_wg_pmlwidlist(CPURISCVState *env, uint32_t pmlwidlist)
+{
+    CPUState *cs = env_cpu(env);
+    RISCVCPU *cpu = RISCV_CPU(cs);
+
+    cpu->cfg.pmlwidlist = pmlwidlist;
+}
+
+void riscv_cpu_set_wg_pmwidlist(CPURISCVState *env, uint32_t pmwidlist)
+{
+    CPUState *cs = env_cpu(env);
+    RISCVCPU *cpu = RISCV_CPU(cs);
+
+    cpu->cfg.pmwidlist = pmwidlist;
+}
+#endif /* CONFIG_USER_ONLY */
+
 /*
  * Returns the effective PMM field.
  *
@@ -162,6 +188,7 @@ bool riscv_env_smode_dbltrp_enabled(CPURISCVState *env, bool virt)
  *     M-mode:  mseccfg.PMM
  *     Other existing modes: disabled
  */
+
 RISCVPmPmm riscv_pm_get_pmm(CPURISCVState *env)
 {
 #ifndef CONFIG_USER_ONLY
