c28e30ad5cc97fe4645a100b5c2534addfb84e5d
Jim Shu <jim.shu@sifive.com>
2026-01-29 15:58:15 +0800

    accel/tcg: Pass access_type as an argument of tlb_set_page*()
    Pass the access_type so that we could know CPU will do the read or
    write access. Then, CPU can fill the CPUTLBEntry[Full] of the specific
    permission (@prot).
    
    It is fine for address_space_translate*() to return different section of
    read and write access. tlb_set_page*() only sets 'CPUTLBEntry.addr_*'
    for specific @prot, so access from another @prot will only get TLB miss
    and start to overwrite 'CPUTLBEntry[Full]' with new @prot.
    
    It is the preliminary patch of next commit to pass the iommu_flags
    to IOMMUMemoryRegion from access_type.
    
    Signed-off-by: Jim Shu <jim.shu@sifive.com>
    Acked-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>

--- accel/tcg/cputlb.c
+++ accel/tcg/cputlb.c
@@ -1023,7 +1023,8 @@ static inline void tlb_set_compare(CPUTLBEntryFull *full, CPUTLBEntry *ent,
  * critical section.
  */
 void tlb_set_page_full(CPUState *cpu, int mmu_idx,
-                       vaddr addr, CPUTLBEntryFull *full)
+                       vaddr addr, MMUAccessType access_type,
+                       CPUTLBEntryFull *full)
 {
     CPUTLB *tlb = &cpu->neg.tlb;
     CPUTLBDesc *desc = &tlb->d[mmu_idx];
@@ -1186,7 +1187,8 @@ void tlb_set_page_full(CPUState *cpu, int mmu_idx,
 
 void tlb_set_page_with_attrs(CPUState *cpu, vaddr addr,
                              hwaddr paddr, MemTxAttrs attrs, int prot,
-                             int mmu_idx, vaddr size)
+                             MMUAccessType access_type, int mmu_idx,
+                             vaddr size)
 {
     CPUTLBEntryFull full = {
         .phys_addr = paddr,
@@ -1196,15 +1198,15 @@ void tlb_set_page_with_attrs(CPUState *cpu, vaddr addr,
     };
 
     assert(is_power_of_2(size));
-    tlb_set_page_full(cpu, mmu_idx, addr, &full);
+    tlb_set_page_full(cpu, mmu_idx, addr, access_type, &full);
 }
 
 void tlb_set_page(CPUState *cpu, vaddr addr,
-                  hwaddr paddr, int prot,
+                  hwaddr paddr, int prot, MMUAccessType access_type,
                   int mmu_idx, vaddr size)
 {
     tlb_set_page_with_attrs(cpu, addr, paddr, MEMTXATTRS_UNSPECIFIED,
-                            prot, mmu_idx, size);
+                            prot, access_type, mmu_idx, size);
 }
 
 /**
@@ -1246,7 +1248,7 @@ static bool tlb_fill_align(CPUState *cpu, vaddr addr, MMUAccessType type,
     if (ops->tlb_fill_align) {
         if (ops->tlb_fill_align(cpu, &full, addr, type, mmu_idx,
                                 memop, size, probe, ra)) {
-            tlb_set_page_full(cpu, mmu_idx, addr, &full);
+            tlb_set_page_full(cpu, mmu_idx, addr, type, &full);
             return true;
         }
     } else {
